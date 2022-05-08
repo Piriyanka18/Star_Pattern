a7b2d9d5f9332a3d74f189762acfc2fe0649589a
Piriyanka Periyathamby <80697383+Piriyanka18@users.noreply.github.com>
2022-05-08 21:28:26 +0530

    Completed

---
+++ src/Lession1/Pattern.java
@@ -0,0 +1,22 @@
+package Lession1;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		for (int i=1;i<=5;i++) {
+			for(int j=0;j<i;j++) {
+				System.out.print("*");	
+			}
+		    System.out.println();
+			
+		}
+	
+	for (int i=1;i<=5;i++) {
+		for(int j=4;j>i;j--) {
+			System.out.print("*");	
+		}
+	    System.out.println();
+		
+	}
+	}
+}
